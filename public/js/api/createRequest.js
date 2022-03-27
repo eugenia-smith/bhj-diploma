ad450130beaa3aaab1a64eb4e8b6737efe0039c3
eugenia-smith <ev-genius4@mail.ru>
2022-03-27 18:20:24 +0300

    First part of createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,35 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest;
+  xhr.responseType = 'json'
 
+  if (options.method == 'GET') {
+    url = options.url
+    // TODO data
+    xhr.open('GET', url, async=true);
+    xhr.send(null);
+  } else {
+    formData = new FormData;
+    // TODO data
+    formData.append( 'hello', 'world' );
+    formData.append( 'iron', 'maiden' );
+    xhr.open( 'POST', options.url, async=true);
+    xhr.send( formData );
+  }
+
+  xhr.onload = () => {
+    if (options.callback) {
+      options.callback(err=null, data=xhr.response) 
+    }
+  }
+  xhr.onerror = () => {
+    console.log("Hello")
+    if (options.callback) {
+      options.callback(err=xhr.status, data=null)
+    }
+    console.log("Bye")
+  }
+
+  return xhr;
 };
